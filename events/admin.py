1d6d9119f6c460ca9b3c9be374a54d4ecf3d1826
Alex Korzh <alex.korzh@hotmail.com>
2022-09-14 08:20:07 +0300

    third commit

--- events/admin.py
+++ events/admin.py
@@ -4,11 +4,78 @@ from . import models
 # Register your models here.
 
 
-@admin.register(models.Event)
-class EventAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'display_event_count', ]
+    list_display_links = ['id', 'title', ]
+
+
+@admin.register(models.Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', ]
+    list_display_links = ['id', 'title', ]
+
+
+@admin.register(models.Enroll)
+class EnrollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event', 'created', ]
+    list_select_related = ['event', ]
+    list_display_links = ['id', 'user', 'event', ]
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'event', 'rate', 'created', 'updated', ]
+    list_display_links = ['id', 'user', 'event', ]
+    list_filter = ['created', 'event', ]
+    readonly_fields = ['created', 'updated', 'id', ]
+
+
+class EventReviewInline(admin.TabularInline):
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_add_permission(self, request, obj):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def get_readonly_fields(self, request, obj=None):
+        return ['id', 'created', 'updated', ]
+    model = models.Review
+    extra = 0
+    fields = ['id', 'user', 'event', 'rate', 'text', 'created', 'updated', ]
+
+
+class PlacesLeftFilter(admin.SimpleListFilter):
+    title = 'Заполненность'
+    parameter_name = 'places_left_filter'
+
+    def lookups(self, request, model_admin):
+        filter_list = (
+            ('0', '<= 50%'),
+            ('1', '> 50%'),
+            ('2', 'sold-out')
+        )
+        return filter_list
+
+    def queryset(self, request, queryset):
+        filter_value = self.value()
+        # if filter_value == '0':
+        #     return queryset.filter(__gt=0, __lte=0.5)
+        # if filter_value == '1':
+        #     return queryset.filter(occupancy_estimation_prop__gt=0.5)
+        # if filter_value == '2':
+        #     return queryset.filter(places_left_prop=0)
+        return queryset
+
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    # TODO Также требуется реализовать собственный фильтр: “Заполненность”
+    list_display = ['id', 'title', 'date_start', 'participants_number', 'is_private', 'category', 'display_enroll_count', 'display_places_left', ]
+    search_fields = ['title', ]
+    list_filter = [PlacesLeftFilter, 'category', 'features', ]
+    ordering = ['date_start', ]
+    fields = ['title', 'description', 'date_start', 'participants_number', 'is_private', 'category', 'features', 'show_enroll_count', 'show_places_left', ]
+    readonly_fields = ['show_enroll_count', 'show_places_left', ]
+    filter_horizontal = ('features', )
+    inlines = [EventReviewInline]
--- events/models.py
+++ events/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
-VALIDATOR = MaxValueValidator
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=90, verbose_name='Категория', default='')
+
+    def display_event_count(self):
+        return self.events.count()
+    display_event_count.short_description = 'Количество событий для категории'
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+        verbose_name = 'Категория'
+
+
+class Feature(models.Model):
+    title = models.CharField(max_length=90, verbose_name='Свойство события', default='')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Свойства событий'
+        verbose_name = 'Свойство события'
 
 
 class Event(models.Model):
@@ -14,15 +40,63 @@ class Event(models.Model):
         validators=[MinValueValidator(0), (MaxValueValidator(10000))]
     )
     is_private = models.BooleanField(verbose_name='Частное', default=False)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_DEFAULT, default='', related_name='events', verbose_name='Категория')
+    features = models.ManyToManyField(Feature, verbose_name='Свойства события')
+
+    def __str__(self):
+        return self.title
+
+    def display_enroll_count(self):
+        return self.enrolls.count()
+    display_enroll_count.short_description = 'Количество записей'
+    show_enroll_count = property(display_enroll_count)
+
+    def places_left(self):
+        return self.participants_number - self.enrolls.count()
+    places_left_prop = property(places_left)
+
+    def occupancy_estimation(self):
+        return self.places_left_prop / self.participants_number
+    occupancy_estimation_prop = property(occupancy_estimation)
+
+    def display_places_left(self):
+        occupancy = ''
+        if self.occupancy_estimation_prop > 0.5:
+            occupancy = '> 50%'
+        elif self.occupancy_estimation_prop <= 0.5 and self.places_left_prop != 0:
+            occupancy = '<= 50%'
+        elif self.places_left_prop == 0:
+            occupancy = 'sold-out'
+        return f'{self.places_left_prop} ({occupancy})'
+    display_places_left.short_description = 'Осталось мест'
+    show_places_left = property(display_places_left)
 
     class Meta:
         verbose_name_plural = 'События'
         verbose_name = 'Событие'
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=90, verbose_name='Категория', default='')
+class Enroll(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_DEFAULT, default='', related_name='enrolls', verbose_name='Пользователь')
+    event = models.ForeignKey(Event, null=True, on_delete=models.SET_DEFAULT, default='', related_name='enrolls', verbose_name='Событие')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     class Meta:
-        verbose_name_plural = 'Категории'
-        verbose_name = 'Категория'
+        verbose_name_plural = 'Записи на события'
+        verbose_name = 'Запись на событие'
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_DEFAULT, default='', related_name='reviews', verbose_name='Пользователь')
+    event = models.ForeignKey(Event, null=True, on_delete=models.SET_DEFAULT, default='', related_name='reviews', verbose_name='Событие')
+    rate = models.PositiveSmallIntegerField(null=True, default='', verbose_name='Оценка пользователя')
+    text = models.TextField(null=True, default='', blank=True, verbose_name='Текст отзыва')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        verbose_name_plural = 'Отзывы'
+        verbose_name = 'Отзыв'
+
+    def __str__(self):
+        return f'{self.user} - {self.rate} - {self.event}'
